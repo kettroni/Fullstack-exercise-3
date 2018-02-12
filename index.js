ca0a25ede57fe77f7b6af0658bb097c5d12ddc35
Roni Kettunen <ronikettunen96@gmail.com>
2018-02-12 14:41:49 +0200

    Exercise3.15 working, trying 3.16

--- index.js
+++ index.js
@@ -61,10 +61,14 @@ app.get('/api/persons/:id', (req,res) => {
 })
 
 app.delete('/api/persons/:id', (req,res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  res.status(204).end()
+  const id = req.params.id
+  Person
+    .findByIdAndRemove(id, (result) => {
+      return res.json(result)
+    })
+    .catch((error) => {
+      return res.status(204).end()
+    })
 })
 
 app.post('/api/persons', (req, res) => {
